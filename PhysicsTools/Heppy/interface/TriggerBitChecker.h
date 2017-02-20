cc236318d3b0464ae3c757c52e0da88373dbb4c2
Giovanni <gpetruc@gmail.com>
2017-02-20 12:19:09 +0100

    Support for prescaling in TriggerBitFilter

--- PhysicsTools/Heppy/interface/TriggerBitChecker.h
+++ PhysicsTools/Heppy/interface/TriggerBitChecker.h
@@ -12,16 +12,19 @@ namespace heppy {
 class TriggerBitChecker {
     public:
         struct pathStruct {
-            pathStruct( const std::string &s ) : pathName(s), first(0),last(99999999) {}
-            pathStruct() : pathName(), first(0),last(99999999) {}
+            pathStruct( const std::string &s ) : pathName(s), first(0),last(99999999),myprescale(0) {}
+            pathStruct() : pathName(), first(0),last(99999999),myprescale(0) {}
             std::string pathName;
             unsigned int first;
             unsigned int last;
+            unsigned int myprescale;
         };
 
         TriggerBitChecker(const std::string &path="DUMMY");
         TriggerBitChecker(const std::vector<std::string> &paths);
         ~TriggerBitChecker() {}
+
+        void myPrescaleTrigger(const std::string &path, unsigned int number) ;
 
         bool check(const edm::EventBase &event, const edm::TriggerResults &result) const ;
         bool check_unprescaled(const edm::EventBase &event, const edm::TriggerResults &result_tr, const pat::PackedTriggerPrescales &result) const ;
@@ -33,6 +36,7 @@ class TriggerBitChecker {
     
         mutable edm::ParameterSetID lastID_;
         mutable std::vector<unsigned int> indices_;
+        mutable std::vector<unsigned int> myprescales_;
 
         /// sync indices with path names
         void syncIndices(const edm::EventBase &event, const edm::TriggerResults &result) const ;
